745775b90d216418de975fd8605360f092b0cd74
Emir-MD <aldomd155@gmail.com>
2024-12-18 10:24:09 -0600

    Updates

---
+++ precache-manifest.0cc003035161f0da3b937764c274e4f6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d2f05b4951a1e0834205f28913cff7",
+    "url": "/restaurante/index.html"
+  },
+  {
+    "revision": "ae690151579ba01c41b6",
+    "url": "/restaurante/static/css/2.b5215549.chunk.css"
+  },
+  {
+    "revision": "5e2837cc249690c2fa4e",
+    "url": "/restaurante/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ae690151579ba01c41b6",
+    "url": "/restaurante/static/js/2.3de6d23a.chunk.js"
+  },
+  {
+    "revision": "035ad5eb2b54dfbf843d",
+    "url": "/restaurante/static/js/3.760a3ae0.chunk.js"
+  },
+  {
+    "revision": "5e2837cc249690c2fa4e",
+    "url": "/restaurante/static/js/main.ac45fdc1.chunk.js"
+  },
+  {
+    "revision": "17230e2a4b0b61d09d5f",
+    "url": "/restaurante/static/js/runtime~main.4d52c070.js"
+  },
+  {
+    "revision": "0f120c7b4da1972cd7df55676d98f959",
+    "url": "/restaurante/static/media/changa-arabic-400-normal.0f120c7b.woff"
+  },
+  {
+    "revision": "651155771ac3c7bc7d819e3a73533970",
+    "url": "/restaurante/static/media/changa-arabic-400-normal.65115577.woff2"
+  },
+  {
+    "revision": "58196d36198a2a3cdd0fa55decc648eb",
+    "url": "/restaurante/static/media/changa-latin-400-normal.58196d36.woff2"
+  },
+  {
+    "revision": "8ebe6f8c08647aeca9bf4bce9cbab5a7",
+    "url": "/restaurante/static/media/changa-latin-400-normal.8ebe6f8c.woff"
+  },
+  {
+    "revision": "3c09450e7d6cfccc60c435f75640f4db",
+    "url": "/restaurante/static/media/changa-latin-ext-400-normal.3c09450e.woff2"
+  },
+  {
+    "revision": "b5ee835841585e9bfe6d18bbd425e8d3",
+    "url": "/restaurante/static/media/changa-latin-ext-400-normal.b5ee8358.woff"
+  },
+  {
+    "revision": "c41b686dfc67beab94ab4e66a99a4f58",
+    "url": "/restaurante/static/media/logos.c41b686d.png"
+  },
+  {
+    "revision": "27cba022d9a8117a73b3d868eea1136b",
+    "url": "/restaurante/static/media/miercoles.27cba022.jpg"
+  },
+  {
+    "revision": "4c6a0c2b2470f72ddb96e49fb920123d",
+    "url": "/restaurante/static/media/paega.4c6a0c2b.jpg"
+  },
+  {
+    "revision": "67463984abee33ba7026998772edfc19",
+    "url": "/restaurante/static/media/paquete.67463984.jpg"
+  },
+  {
+    "revision": "42d8a788393ad890b3bc30a5a2bdfd6a",
+    "url": "/restaurante/static/media/poppins-latin-400-normal.42d8a788.woff"
+  },
+  {
+    "revision": "9212f6f9860f9fc6c69b02fedf6db8c3",
+    "url": "/restaurante/static/media/poppins-latin-400-normal.9212f6f9.woff2"
+  },
+  {
+    "revision": "74cfaf5cbbc865ffd2c2ac84dbc85681",
+    "url": "/restaurante/static/media/poppins-latin-ext-400-normal.74cfaf5c.woff"
+  },
+  {
+    "revision": "aa42a9a3d4fc9951ed37945ff1af85dc",
+    "url": "/restaurante/static/media/poppins-latin-ext-400-normal.aa42a9a3.woff2"
+  },
+  {
+    "revision": "60401810e53a497b80afa1b2283c56f0",
+    "url": "/restaurante/static/media/promo.60401810.png"
+  },
+  {
+    "revision": "ac53f3d6a797f969c26ac0026ddee516",
+    "url": "/restaurante/static/media/promos.ac53f3d6.png"
+  }
+]);
